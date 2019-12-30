611f08beae0313de2ac2497aee5b0f49eedd3ac4
IL KWON KIM <magictical@gmail.com>
2019-12-30 18:17:46 +0900

    add tab Bar

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:section3_mealapp/screens/categories_screen.dart';
+import 'package:section3_mealapp/screens/favorites_screen.dart';
+
+class TabScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[CategoriesScreen(), FavoritesScreen()],
+        ),
+      ),
+    );
+  }
+}

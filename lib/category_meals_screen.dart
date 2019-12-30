de03dea2305e51855cde154cb47f345295761812
IL KWON KIM <magictical@gmail.com>
2019-12-30 11:52:00 +0900

    display the dummy data to recipes screen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -13,13 +14,20 @@ class CategoryMealsScreen extends StatelessWidget {
 
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes For THe Category!'),
+      // builder for better performance in larger data
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return (Text(categoryMeals[index].title));
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }

2b6c9204a7cb01efa60a67799130c11b0908b1a4
IL KWON KIM <magictical@gmail.com>
2019-12-31 17:18:29 +0900

    use pushReplacementNamed instead of pushNamed()

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:section3_mealapp/widget/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+      // use hamburger icon
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters!'),
+      ),
     );
   }
 }
